0e185ecf609621ebd26c909df2e1eccf3a9fdfe7
zoomer <zoomer@laptop.com>
2025-12-07 21:25:46 +0100

    add resurrect/continuum tmux plugins

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -11,6 +11,14 @@
     customPaneNavigationAndResize = true;
     plugins = with pkgs; [
       tmuxPlugins.nord
+      tmuxPlugins.resurrect
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '15' # Save every 15 minutes
+        '';
+      }
     ];
   };
 }
