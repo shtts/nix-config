3ae43f0aecafb9889310caf931d19f93ba958f65
zoomer <zoomer@laptop.com>
2025-12-07 21:19:08 +0100

    add tmux options

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -2,6 +2,13 @@
 {
   programs.tmux = {
     enable = true;
+    escapeTime = 0;
+    mouse = true;
+    baseIndex = 1;
+    historyLimit = 100000;
+    disableConfirmationPrompt = true;
+    keyMode = "vi";
+    customPaneNavigationAndResize = true;
     plugins = with pkgs; [
       tmuxPlugins.nord
     ];
