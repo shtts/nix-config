4ca9bd146bbe5690750458bb5f0c7685439fd733
zoomer <zoomer@laptop.com>
2025-12-07 01:44:06 +0100

    fix syntax error in foot.nix

--- modules/foot.nix
+++ modules/foot.nix
@@ -4,10 +4,10 @@
     enable = true;
     settings = {
       main = {
+        font = "JetBrainsMono Nerd Font Mono:size=12";
         pad = "10x10";
         include = "/nix/store/0crnh6a2j96izghj1kw05dlgy8ag598r-foot-1.25.0-themes/share/foot/themes/nord";
       };
-      font = "JetBrainsMono Nerd Font Mono:size=12";
     };
   };
 }
