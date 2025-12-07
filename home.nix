f877c0175443c9ac7fe0ba31c3de21ba644716cc
zoomer <zoomer@laptop.com>
2025-12-07 19:36:28 +0100

    add obsidian.nix to imports

--- home.nix
+++ home.nix
@@ -22,7 +22,7 @@
     ./modules/foot.nix
     ./modules/kitty.nix
     ./modules/dms.nix
-    ./obsidian.nix
+    ./modules/obsidian.nix
   ];
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
