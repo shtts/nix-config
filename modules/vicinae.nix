9bf2fac37c33528005e9901dae1ca8d8b7551456
zoomer <zoomer@laptop.com>
2025-12-07 03:27:31 +0100

    removeing unnecessary lines from vicinae.nix

--- modules/vicinae.nix
+++ modules/vicinae.nix
@@ -8,16 +8,9 @@
     enable = true;
   };
 
-  systemd.user.services.vicinae = {
-    Unit = {
-      Description = "Vicinae Server";
-      After = [ "graphical-session.target" ];
-    };
-
-    home.activation = {
-      restartVicinae = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  home.activation = {
+    restartVicinae = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       $DRY_RUN_CMD nohup ${pkgs.vicinae}/bin/vicinae server --replace >/dev/null 2>&1 & disown
-      '';
-    };
+    '';
   };
 }
