471a5b9cc9455a5a7fabd1dd05e71e3f99d098a9
zoomer <zoomer@laptop.com>
2025-12-08 15:24:44 +0100

    allow unfree software 'obsidian'

--- home.nix
+++ home.nix
@@ -75,6 +75,12 @@
     # '')
   ];
 
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "obsidian"
+    ];
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
