3ed0d576ad969433325543f28b3bcc29ff48e943
zoomer <zoomer@laptop.com>
2025-12-08 03:51:25 +0100

    disable nord theme in obsidian, because it looks ugly

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -17,21 +17,21 @@
               }
             ];
           };
-          appearance = {
-            cssTheme = "Nord";
-          };
-          themes = [
-            {
-              pkg = pkgs.fetchFromGitHub {
-                owner = "insanum";
-                repo = "obsidian_nord";
-                rev = "master";
-                hash = "sha256-LdjxEpxnsV4YXI9K72o2m6E1nhZvKb/r/WCuVKihugI=";
-                name = "Nord";
-              };
-              enable = true;
-            }
-          ];
+          # appearance = {
+          #   cssTheme = "Nord";
+          # };
+          # themes = [
+          #   {
+          #     pkg = pkgs.fetchFromGitHub {
+          #       owner = "insanum";
+          #       repo = "obsidian_nord";
+          #       rev = "master";
+          #       hash = "sha256-LdjxEpxnsV4YXI9K72o2m6E1nhZvKb/r/WCuVKihugI=";
+          #       name = "Nord";
+          #     };
+          #     enable = true;
+          #   }
+          # ];
 
         };
       };
