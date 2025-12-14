826a7b2e9fe006aa6cb592c1dab24884226e045f
zoomer <zoomer@laptop.com>
2025-12-14 03:14:48 +0100

    a bit of formating

--- HMmodules/kitty.nix
+++ HMmodules/kitty.nix
@@ -5,18 +5,16 @@
     themeFile = "Nord";
 
     font = {
-      name =  "JetBrainsMono Nerd Font";
+      name = "JetBrainsMono Nerd Font";
       size = 12;
     };
 
-
-keybindings = {
-    "ctrl+shift+l" = "next_tab";
-    "ctrl+shift+h"  = "previous_tab";
-    "ctrl+shift+t"     = "new_tab";
-    "ctrl+shift+q"     = "close_tab";
-    
-  };
+    keybindings = {
+      "ctrl+shift+l" = "next_tab";
+      "ctrl+shift+h" = "previous_tab";
+      "ctrl+shift+t" = "new_tab";
+      "ctrl+shift+q" = "close_tab";
+    };
     settings = {
       background_opacity = lib.mkForce "0.9";
       window_padding_width = 10;
