3a765ee3686ee380cdc1d4f58dfeac7a85fb2bee
zoomer <zoomer@laptop.com>
2025-12-29 04:37:01 +0100

    configure vicinae

--- HMmodules/vicinae.nix
+++ HMmodules/vicinae.nix
@@ -1,11 +1,24 @@
 {
   pkgs,
-  lib,
+  config,
   ...
 }:
 {
   programs.vicinae = {
     enable = true;
+    extensions = [
+      (config.lib.vicinae.mkExtension {
+        name = "swww-switcher";
+        src = pkgs.fetchFromGitHub {
+          owner = "ViSovereign";
+          repo = "swww-switcher";
+          rev = "4726bb460107e3454e2e7541a9e392461b98d186";
+          hash = "sha256-4zIvt7PR7qqbP+O1EHmRjy6nCtpZnAVdf8Al61gwA7A=";
+        };
+      })
+    ];
+    settings = {
+      theme = "Nord";
+    };
   };
-
 }
